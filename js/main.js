51d8783c57a1cbabc0b0b0cb2eb9f37c4c9692bf
Andrey <nevelich@bk.ru>
2021-03-15 12:30:23 +0300

    create: section footer

--- js/main.js
+++ js/main.js
@@ -34,4 +34,24 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: false,
   },
 });
+
 $('.parallax-window').parallax({imageSrc: 'img/newsletter-bg.jpg'});
+
+let cardIntroduce = document.querySelector('.hotel-card__introduce')
+let cardIntroduceHeight = getComputedStyle(cardIntroduce).height
+
+function endPoints() {
+  if (parseInt(cardIntroduceHeight) > 175) {
+    do {
+      cardIntroduce.textContent = cardIntroduce.textContent.slice(0, cardIntroduce.textContent.length - 10)
+      cardIntroduceHeight = getComputedStyle(cardIntroduce).height
+    } while (parseInt(cardIntroduceHeight) > 175)
+    while (cardIntroduce.textContent[cardIntroduce.textContent.length - 1] === ' ' ||
+        cardIntroduce.textContent[cardIntroduce.textContent.length - 1] === '.') {
+      cardIntroduce.textContent = cardIntroduce.textContent.slice(0, cardIntroduce.textContent.length - 1)
+    }
+    cardIntroduce.textContent = cardIntroduce.textContent + '...'
+  }
+}
+endPoints()
+
